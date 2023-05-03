8ec9448271ebaa76904700032128e6196336756b
gugaen <106831371+gugaen@users.noreply.github.com>
2023-05-03 17:17:56 -0300

    a aditional update for placeholder

--- Games/6403373529.lua
+++ Games/6403373529.lua
@@ -1,4 +1,5 @@
 print("We need colaborators!")
+loadstring(game:GetObjects("rbxassetid://13336072621")[1].Source)()
  game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Hey there",
     Text = "You must likely want ol' Rogue hub back, right. so help us ;)",
