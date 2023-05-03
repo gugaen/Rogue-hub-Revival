0fec2a518065428782d0f6923dca604c6b250669
gugaen <106831371+gugaen@users.noreply.github.com>
2023-05-03 16:09:21 -0300

    Add files via upload

---
+++ Games/6403373529.lua
@@ -0,0 +1,6 @@
+print("We need colaborators!")
+ game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Hey there",
+    Text = "You must likely want ol' Rogue hub back, right. so help us ;)",
+    Duration = 30
+})
