f5c7c1d6810043c3ebefad34edf30587b9b1dd61
gugaen <106831371+gugaen@users.noreply.github.com>
2023-07-06 14:32:28 -0300

    Hambugaer
    aaaa

--- Games/6403373529.lua
+++
@@ -1,7 +0,0 @@
-print("We need colaborators!")
-game:GetService("InsertService"):LoadLocalAsset("rbxassetid://13336072621", game.Workspace)
- game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Hey there",
-    Text = "You must likely want ol' Rogue hub back, right. so help us ;)",
-    Duration = 30
-})
